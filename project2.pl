eb04204b0d5ccc4cbf816d1bc7f60c4c31dbce09
cyril-casapao <casapaoc@gmail.com>
2016-03-09 12:50:11 -0500

    Add project2.pl source file.

---
+++ project2.pl
@@ -0,0 +1,19 @@
+/**
+* project2.pl
+*
+* This program contains a database of GVSU CIS faculty and the classes they
+* teach. It also contains rules that answer the following questions:
+*
+*   1) What does Dr. J. Leidig teach?
+*   2) Does Dr. J. Leidig teach Database?
+*   3) What is Dr. J. Leidig's schedule?
+*   4) Who is scheduled to teach what subject on TTH, 10AM?
+*   5) When do Dr. J. Leidig and Dr. El-Said teach at the same time?
+*   6) Who teaches at the same time as Dr. J. Leidig?
+*   7) What courses do Jim and Pam have in common?
+*   8) Who is taking CS courses?
+*   9) What types of courses is Gaius Baltar taking?
+*   10) Are there any scheduling conflicts of profs or locations?
+*
+* @author Kevin Anderson & Cyril Casapao
+*/
